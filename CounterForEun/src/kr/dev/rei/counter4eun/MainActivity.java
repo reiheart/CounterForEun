8fdcb50d0405a8f4821493e3879c1c1c9f1f7e1f
Park Kyoung Hee <reiheart@gmail.com>
2012-12-29 15:33:24 +0900

    added new feature
    Signed-off-by: Park Kyoung Hee <reiheart@gmail.com>

--- CounterForEun/src/kr/dev/rei/counter4eun/MainActivity.java
+++ CounterForEun/src/kr/dev/rei/counter4eun/MainActivity.java
@@ -6,15 +6,18 @@ import android.content.SharedPreferences;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnFocusChangeListener;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends Activity implements OnClickListener {
+public class MainActivity extends Activity implements OnClickListener, OnFocusChangeListener {
 	Button buttonUp;
 	Button buttonDown;
-	Button buttonReset;
+	Button buttonResetCount;
+	Button buttonResetMemo;
 	
 	EditText textCount;
+	EditText textDescription;
 
 	SharedPreferences sharedPref;
 	SharedPreferences.Editor sharedPrefEditor;
@@ -25,18 +28,33 @@ public class MainActivity extends Activity implements OnClickListener {
 		setContentView(R.layout.activity_main);
 		
 		textCount = (EditText) findViewById(R.id.textCount);
+		textDescription = (EditText) findViewById(R.id.textDescription);
 		buttonUp = (Button) findViewById(R.id.buttonUp);
 		buttonDown = (Button) findViewById(R.id.buttonDown);
-		buttonReset = (Button) findViewById(R.id.buttonReset);
+		buttonResetCount = (Button) findViewById(R.id.buttonResetCount);
+		buttonResetMemo = (Button) findViewById(R.id.buttonResetMemo);
 		
+		textDescription.setOnFocusChangeListener(this);
 		buttonUp.setOnClickListener(this);
 		buttonDown.setOnClickListener(this);
-		buttonReset.setOnClickListener(this);
+		buttonResetCount.setOnClickListener(this);
+		buttonResetMemo.setOnClickListener(this);
 		
 		sharedPref = getSharedPreferences("CounterForEun", Activity.MODE_PRIVATE);
 		sharedPrefEditor = sharedPref.edit();
 		int countingValue = sharedPref.getInt("CountingValue", 0);
 		textCount.setText(String.valueOf(countingValue));
+		String descStr = sharedPref.getString("Description", "");
+		textDescription.setText(descStr);
+	}
+
+	@Override
+	protected void onPause() {
+		String descStr = textDescription.getText().toString();
+		sharedPrefEditor.putString("Description", descStr);
+		sharedPrefEditor.commit();
+		
+		super.onPause();
 	}
 
 	@Override
@@ -63,10 +81,26 @@ public class MainActivity extends Activity implements OnClickListener {
 			sharedPrefEditor.putInt("CountingValue", countingValue);
 			sharedPrefEditor.commit();
 		}
-		else if (v.equals(buttonReset))
+		else if (v.equals(buttonResetCount))
 		{
 			textCount.setText("0");
 			sharedPrefEditor.putInt("CountingValue", 0);
+			sharedPrefEditor.commit();
+		}
+		else if (v.equals(buttonResetMemo))
+		{
+			textDescription.setText("");
+			sharedPrefEditor.putString("Description", "");
+			sharedPrefEditor.commit();	
+		}
+	}
+
+	@Override
+	public void onFocusChange(View v, boolean hasFocus) {
+		if (v.equals(textDescription))
+		{
+			String descStr = textDescription.getText().toString();
+			sharedPrefEditor.putString("Description", descStr);
 			sharedPrefEditor.commit();
 		}
 	}
